8c03d6d848445bd60e1100ec64f2d9d5a06a0e3b
Anushka Walia <anushkawalia64@gmail.com>
2026-01-29 21:34:33 +0530

    Configured settings for deployment

--- petsitting/petsitting/settings.py
+++ petsitting/petsitting/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%^_e)+f&j17@ap!9e91fq*-h+s5s#lcfo!fsml6l24ln=iy2*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definition
